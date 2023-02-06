fe8965e248de6f0f01141525b5ff8520af94ecbd
Kamlesh Pradhan <kamlesh.pradhan@turnoutnow.com>
2023-02-06 20:09:57 +0530

    Updates in the test.js and index.js file

--- index.js
+++ index.js
@@ -5,31 +5,31 @@ const {default:rockset} = require("@rockset/client");
 // const rockset = require("@rockset/client").default;
 
 const apikey = process.env.ROCKSET_APIKEY;
-const rocksetClient = rockset(apikey);
-rocksetClient.queries.query({
-        sql: {
-            query: 'SELECT * FROM dev.dev_tn_main clear ORDER BY activityId ASC',
-            generate_warnings: 'true',
-            parameters: [{
-                name: '_id',
-                type: 'string',
-                value: 'e2a2d01b-2fa4-7002-e347-13264662c14e'
-            },{
-                name:'sentiment',
-                type:'string',
-                value:'sentiment'}],
-            default_row_limit: '123',
-            paginate: 'true',
-            initial_paginate_response_doc_count: '123'
-        },
-        async_options: {
-            client_timeout_ms: '123',
-            timeout_ms: '123',
-            max_initial_results: '123'
-        }
-    })
-    .then(console.log)
-    .catch(console.error);
+const rocksetClient = rockset('NiJ6NGrjL8yhMM5dr9ohZE8SrBh3tGsLkCbBuE7RO6Gk1z35h24lbLnXMwgOKJIg', "https://api.usw2a1.rockset.com");
+// rocksetClient.queries.query({
+//         sql: {
+//             query: 'SELECT * FROM dev.dev_tn_pc ORDER BY name DESC LIMIT 10',
+//             generate_warnings: 'true',
+//             parameters: [{
+//                 name: '_id',
+//                 type: 'string',
+//                 value: 'b011ee5f-72bf-9405-2d78-f85d72a9d9e4'
+//             },{
+//                 name:'name',
+//                 type:'string',
+//                 value:'name'}],
+//             default_row_limit: '123',
+//             paginate: 'true',
+//             initial_paginate_response_doc_count: '123'
+//         },
+//         async_options: {
+//             // client_timeout_ms: '123',
+//             // timeout_ms: '123',
+//             max_initial_results: '123'
+//         }
+//     })
+//     .then(console.log)
+//     .catch(console.error);
 // const apiserver = process.env.apiServer;
 // const rocksetClient = rockset(apikey, apiserver);
 // console.log(rocksetClient.queryLambdas.executeQueryLambda)
@@ -37,3 +37,11 @@ rocksetClient.queries.query({
 //   .then(console.log)
 //   .catch(console.log);
     
+
+rocksetClient.queryLambdas
+  .executeQueryLambdaByTag('dev', 'tn_pc-getAllMeetings', 'latest', {
+    /* (optional) list of parameters that may be used in the query, that overwrite default parameters */
+    parameters: [{ name: 'eventId', type: 'string', value: '21kOONLxRlEofMY47gxWCBGxTzW' }],
+  })
+  .then(console.log)
+  .catch(console.error);
---
+++ test.js
@@ -0,0 +1,62 @@
+// Import with es6
+require('dotenv').config()
+const {default:rockset} = require("@rockset/client");
+const axios = require("axios");
+
+
+const getData = async()=>{
+    try{
+    const response = await axios({
+        method:"post",
+        url:"https://api.usw2a1.rockset.com/v1/orgs/self/ws/dev/lambdas/tn_pc-getAllMeetings/tags/latest ",
+        headers:{
+                "Authorization": 'ApiKey NiJ6NGrjL8yhMM5dr9ohZE8SrBh3tGsLkCbBuE7RO6Gk1z35h24lbLnXMwgOKJIg',
+                "Content-Type": 'application/json' 
+                },
+     
+        data:{
+            "parameters": [
+              {
+                "name": "eventId",
+                "type": "string",
+                "value": "21kOONLxRlEofMY47gxWCBGxTzW"
+              }
+            ]
+        
+          }
+    })
+    console.log(response.data.results);
+}catch(err){
+    console.log(err)
+}
+    
+}
+getData();
+
+
+// {
+//     "sql": {
+//       "query": "SELECT * FROM dev.dev_tn_pc_v2  WHERE eventId = :eventId",
+//       "generate_warnings": "true",
+//       "parameters": [
+//         {
+//           "name": "_id",
+//           "type": "string",
+//           "value": "e978d63e-f9e4-48cb-5f89-814cc5a9c18a"
+//         }
+//           , {
+//           "name": "eventId",
+//           "type": "string",
+//           "value": "W1JNg-ImIaOn355AmSVya"
+//         }
+//       ],
+//       "default_row_limit": "123",
+//       "paginate": "true",
+//       "initial_paginate_response_doc_count": "123"
+//     },
+//     "async_options": {
+//       "client_timeout_ms": "123",
+//       "timeout_ms": "123",
+//       "max_initial_results": "123"
+//     }
+// }
